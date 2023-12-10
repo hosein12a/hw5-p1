c02dbe110e833beac10c48e2bbf36d5f579c8b06
hosein12a <153413720+hosein12a@users.noreply.github.com>
2023-12-09 21:48:46 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main() {
+float n,h,r;
+cout << "please enter n=";
+cin>>n;
+
+if (n<6000000) {
+	cout<<n;
+}	
+
+else if (n>=6000000&&n<8000000)	{
+	h=n*0.5;
+}
+
+else if (n>=8000000&&n<10000000) {
+	h=n*0.10;
+}
+
+else if (n>=10000000&&n<14000000) {
+	h=n*0.15;
+}
+    
+else if (n>=14000000&&n<18000000){
+	h=n*0.20;
+}
+
+else if (n>=18000000&&n<25000000){
+	h=n*0.25;
+}
+
+else if (n<25000000) {
+	h=n*0.35;
+}
+
+cout<<"tax.h="<<h<<"\n";
+r=n-h;
+
+cout<<"total salaries with deductions is="<<r;
+
+	
+}
